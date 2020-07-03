37776ad24083b8bab3677a00d1667030441755cb
k1005sh <djenadml1005@naver.com>
2020-07-04 01:26:11 +0900

    state study

--- src/App.js
+++ src/App.js
@@ -1,51 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import { render } from '@testing-library/react';
-
-class Subject extends Component {
-  render(){
-  return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render(){
-  return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render(){
-  return (
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
-      </article>
-    );
-  }
-}
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 
 class App extends Component {
+  //초기화 시켜주고 싶은 것들을 cons안에 코딩한다
+  //state를 사용하면서 정보를 은닉함
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World wide web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render(){
   return (
       <div className="App">
-        <Subject title="WEB" sub="world wide WEB!"></Subject>
-        <Subject title="Second" sub="React"></Subject>
-        <TOC></TOC>
-        <Content></Content>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
