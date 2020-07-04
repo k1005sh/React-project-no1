2a574a647f96e7cfce3873c460c2e07c9db2f761
k1005sh <djenadml1005@naver.com>
2020-07-04 22:15:00 +0900

    리액트 강좌 16번까지 완료

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode:"welcome",
       subject:{title:'WEB', sub:'World wide web!'},
+      welcome:{title:'Welcome', desc:'Hello, React'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -18,15 +20,33 @@ class App extends Component {
       ]
     }
   }
+  handleClick = (e) =>{
+    e.preventDefault();
+    this.setState({
+      mode:'welcome'
+    });
+  }
   render(){
-  return (
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
       <div className="App">
-        <Subject
+        {/* <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
+        <header>
+          <h1><a href="/" onClick={this.handleClick}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
