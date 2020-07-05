1fd6637d6f76bb512754d5f1cdaeac08b12ad0f8
k1005sh <djenadml1005@naver.com>
2020-07-05 21:18:48 +0900

    18번까지 완료

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode:"welcome",
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World wide web!'},
       welcome:{title:'Welcome', desc:'Hello, React'},
       contents:[
@@ -20,32 +21,51 @@ class App extends Component {
       ]
     }
   }
-  handleClick = (e) =>{
-    e.preventDefault();
-    this.setState({
-      mode:'welcome'
-    });
-  }
+  // handleClick = (e) =>{
+  //   e.preventDefault();
+  //   this.setState({
+  //     mode:'welcome'
+  //   });
+  // }
   render(){
     let _title, _desc = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     }else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      let i = 0;
+      while(i < this.state.contents.length){
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i++;
+      }
     }
     return (
       <div className="App">
-        {/* <Subject
+        <Subject
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
+          sub={this.state.subject.sub}
+          onChangePage = {function(){
+            this.setState({mode:"welcome"});
+          }.bind(this)}
+        >
+        </Subject>
+        {/* <header>
           <h1><a href="/" onClick={this.handleClick}>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}
-        </header>
-        <TOC data={this.state.contents}></TOC>
+        </header> */}
+        <TOC
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+          });
+          }.bind(this)}
+          data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
+//props는 Read-only
 class Subject extends Component {
-    render(){
-    return (
-        <header>
-          <h1><a href="/">{this.props.title}</a></h1>
-          {this.props.sub}
-        </header>
-      );
-    }
+  render(){
+  return (
+      <header>
+        <h1><a href="/" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangePage();
+        }.bind(this)}>{this.props.title}</a></h1>
+        {this.props.sub}
+      </header>
+    );
   }
+}
 
 export default Subject;
